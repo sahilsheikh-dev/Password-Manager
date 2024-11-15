760bb11229166870f36ad1303a23b08f9538990c
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2024-11-15 18:20:46 +0530

    Created Models for Users and Passwords

--- src/main/java/com/begawo/passwordManager/model/Passwords.java
+++ src/main/java/com/begawo/passwordManager/model/Passwords.java
@@ -1,5 +1,115 @@
 package com.begawo.passwordManager.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Passwords {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "password_", nullable = false)
+	private int passwordId;
+	@Column(name = "site_name", nullable = false)
+	private String siteName;
+	@Column(name = "site_username", nullable = false)
+	private String siteUsername;
+	@Column(name = "site_passworrd", nullable = false)
+	private String sitePassword;
+	@Column(name = "site_email", nullable = false)
+	private String siteEmail;
+	@Column(name = "site_url", nullable = false)
+	private String siteUrl;
+
+	@ManyToOne
+	private Users users;
+
+	public Passwords(int passwordId, String siteName, String siteUsername, String sitePassword, String siteEmail,
+			String siteUrl, Users users) {
+		super();
+		this.passwordId = passwordId;
+		this.siteName = siteName;
+		this.siteUsername = siteUsername;
+		this.sitePassword = sitePassword;
+		this.siteEmail = siteEmail;
+		this.siteUrl = siteUrl;
+		this.users = users;
+	}
+
+	public Passwords(String siteName, String siteUsername, String sitePassword, String siteEmail, String siteUrl,
+			Users users) {
+		super();
+		this.siteName = siteName;
+		this.siteUsername = siteUsername;
+		this.sitePassword = sitePassword;
+		this.siteEmail = siteEmail;
+		this.siteUrl = siteUrl;
+		this.users = users;
+	}
+
+	public Passwords() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getPasswordId() {
+		return passwordId;
+	}
+
+	public void setPasswordId(int passwordId) {
+		this.passwordId = passwordId;
+	}
+
+	public String getSiteName() {
+		return siteName;
+	}
+
+	public void setSiteName(String siteName) {
+		this.siteName = siteName;
+	}
+
+	public String getSiteUsername() {
+		return siteUsername;
+	}
+
+	public void setSiteUsername(String siteUsername) {
+		this.siteUsername = siteUsername;
+	}
+
+	public String getSitePassword() {
+		return sitePassword;
+	}
+
+	public void setSitePassword(String sitePassword) {
+		this.sitePassword = sitePassword;
+	}
+
+	public String getSiteEmail() {
+		return siteEmail;
+	}
+
+	public void setSiteEmail(String siteEmail) {
+		this.siteEmail = siteEmail;
+	}
+
+	public String getSiteUrl() {
+		return siteUrl;
+	}
+
+	public void setSiteUrl(String siteUrl) {
+		this.siteUrl = siteUrl;
+	}
+
+	public Users getUsers() {
+		return users;
+	}
+
+	public void setUsers(Users users) {
+		this.users = users;
+	}
 
 }
--- src/main/java/com/begawo/passwordManager/model/Users.java
+++ src/main/java/com/begawo/passwordManager/model/Users.java
@@ -1,5 +1,91 @@
 package com.begawo.passwordManager.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable = false)
+	private int userId;
+	@Column(name = "user_name", nullable = false)
+	private String userName;
+	@Column(name = "user_username", nullable = false)
+	private String userUsername;
+	@Column(name = "user_password", nullable = false)
+	private String userPassword;
+
+	@OneToMany(mappedBy = "user")
+	private List<Passwords> passwords;
+
+	public Users(int userId, String userName, String userUsername, String userPassword, List<Passwords> passwords) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.userUsername = userUsername;
+		this.userPassword = userPassword;
+		this.passwords = passwords;
+	}
+
+	public Users(String userName, String userUsername, String userPassword, List<Passwords> passwords) {
+		super();
+		this.userName = userName;
+		this.userUsername = userUsername;
+		this.userPassword = userPassword;
+		this.passwords = passwords;
+	}
+
+	public Users() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserUsername() {
+		return userUsername;
+	}
+
+	public void setUserUsername(String userUsername) {
+		this.userUsername = userUsername;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public List<Passwords> getPasswords() {
+		return passwords;
+	}
+
+	public void setPasswords(List<Passwords> passwords) {
+		this.passwords = passwords;
+	}
 
 }
